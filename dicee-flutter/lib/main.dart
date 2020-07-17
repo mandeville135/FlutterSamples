6d4f433cc084ccee11109ac6986755032cdbe4c0
mandeville135@gmail.com <mandeville135@gmail.com>
2020-07-16 21:49:01 -0400

    Adding 8 ball code, finishing dice

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,18 +16,31 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void roll(){
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    var leftDiceNumber = 5;
-    var rightDiceNumber = 1;
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
             child: FlatButton(
               onPressed: (){
-                print('.');
+                  roll();
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
@@ -34,7 +48,7 @@ class DicePage extends StatelessWidget {
           Expanded(
             child: FlatButton(
               onPressed: (){
-                print('Right Button Got Pressed.');
+                  roll();
               },
               child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
