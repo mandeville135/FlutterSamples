d635d04e53179ce861913c9ea4235ec340c7fcff
mandeville135@gmail.com <mandeville135@gmail.com>
2020-07-22 19:42:46 -0400

    Finishing most of the weather app

---
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+const apiKey = '2bf5142d91fde3bea8ad3602cd879353';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+    print('InitStateCalled');
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+  }
+
+  void getLocationData() async {
+    Location location = Location();
+    await location.getLocation();
+    longitude = location.longitude;
+    latitude = location.latitude;
+
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric');
+    var weatherData = await networkHelper.getData();
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(weatherData);
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
+  }
+}
---
+++ Clima-Flutter/lib/services/location.dart
@@ -0,0 +1,11 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double longitude;
+  double latitude;
+
+  Future<void> getLocation() async{
+    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    latitude = position.latitude;
+    longitude = position.longitude;
+  }}
